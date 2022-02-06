64ef28cb4f35a44440e95da9c7f780f1208a7fe4
mohamed085 <mohamed085555@gmail.com>
2022-02-06 12:55:39 +0200

    Add application properties and permit all requests

---
+++ src/main/java/com/molla/controller/AdminController.java
@@ -0,0 +1,20 @@
+package com.molla.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("")
+    public String getHomePage() {
+        log.debug("AdminController | GetHomePage");
+
+        return "admin/index";
+    }
+
+}
