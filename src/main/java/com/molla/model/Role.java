6d10a49762e0d57843258069fd5d012c5c5f1001
mohamed085 <mohamed085555@gmail.com>
2022-02-06 13:16:03 +0200

    Add Role entity

---
+++ src/main/java/com/molla/model/Role.java
@@ -0,0 +1,24 @@
+package com.molla.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+
+}
