4157949b85465483bf3db5ea511a907b78c4c79c
mohamed085 <mohamed085555@gmail.com>
2022-02-06 12:42:05 +0200

    Initial commit

---
+++ src/test/java/com/molla/MolladECommercerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolladECommercerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
